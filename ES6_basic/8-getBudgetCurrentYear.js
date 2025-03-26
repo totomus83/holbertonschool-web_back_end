3cd6912c9ff7b65fd3c3b84bdc97d665da5fe4e5
Tom <tom.dibellonio@gmail.com>
2025-03-26 13:28:45 +0100

    test

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -10,6 +10,6 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
     [`income-${year}`]: income,
     [`gdp-${year}`]: gdp,
     [`capita-${year}`]: capita,
-		    };
+  };
   return budget;
 }
