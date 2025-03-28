cb5072a59c004e51794f77b2df5f65397c7c7cda
Tom <tom.dibellonio@gmail.com>
2025-03-28 13:51:58 +0100

    test

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,7 +1,7 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-          throw new TypeError('Name must be a string');
+      throw new TypeError('Name must be a string');
     }
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
