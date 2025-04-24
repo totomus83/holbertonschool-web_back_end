f4a1103efccc712235afd4329e59350f7c994fb1
totomus <tom.dibellonio@gmail.com>
2025-04-24 17:52:32 +0200

    documentation error

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-from typing import List
+
 """
 type annotated function that return sum of a list of floats
 """
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
