f117ffc3d193dc9170a708beb3f7ddb660a31298
totomus <tom.dibellonio@gmail.com>
2025-04-24 17:23:58 +0200

    test

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+the module serves as basic example for variable annotation
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
