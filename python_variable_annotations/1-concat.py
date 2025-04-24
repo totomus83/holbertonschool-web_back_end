f042b3f385893c8195fd8491bf3893ae9d309fe9
totomus <tom.dibellonio@gmail.com>
2025-04-24 17:08:03 +0200

    pycodestyle adjustement

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,2 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module contains a type-annotated function `concat`
+that returns concaconate 2 strings.
+"""
+
+
 def concat(a: str, b:str) -> str:
+    """concaconate 2 str"""
     return a + b
