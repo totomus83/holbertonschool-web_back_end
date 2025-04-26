c740d26984d7c990547ef099fa374527782f9e7c
totomus <tom.dibellonio@gmail.com>
2025-04-26 21:07:29 +0200

    test

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,23 @@
+"""
+Take the code from wait_n and alter it into a new function task_wait_n
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    task_wait_n func
+    """
+    delays = []
+    coroutines = []
+    for number_coroutines in range(n):
+        coroutines.append(task_wait_random(max_delay))
+
+    for coroutine in asyncio.as_completed(coroutines):
+        # as_completed order coroutines by end of excecution (fastest first)
+        delay = await coroutine
+        delays.append(delay)
+
+    return delays
