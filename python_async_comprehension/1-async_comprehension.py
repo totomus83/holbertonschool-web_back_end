9c0cc9868600c2ae55a6107f12059b4ff7ce234b
totomus <tom.dibellonio@gmail.com>
2025-05-01 17:39:46 +0200

    error

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,10 +3,12 @@
 The coroutine will collect 10 random numbers using an async comprehensing
 over async_generator then return the 10 random numbers.
 """
+import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """coroutine called async_comprehension that takes no arguments."""
     return[i async for i in async_generator()]
