bcf55bc878613652d69c19ab192732376721e42a
totomus <tom.dibellonio@gmail.com>
2025-05-01 17:37:41 +0200

    fixed error

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,10 +6,11 @@ then yield a random number between 0 and 10
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ coroutine called async_generator"""
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
