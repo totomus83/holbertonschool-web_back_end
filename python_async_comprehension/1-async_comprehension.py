66ee2e63a85d2260f8d7483071f8ccf76d54abe1
totomus <tom.dibellonio@gmail.com>
2025-05-01 17:41:52 +0200

    test

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,9 @@ over async_generator then return the 10 random numbers.
 """
 import asyncio
 from typing import List
-
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """coroutine called async_comprehension that takes no arguments."""
-    return[i async for i in async_generator()]
+    """coroutine called async_comprehension that takes no arguments"""
+    return [i async for i in async_generator()]
