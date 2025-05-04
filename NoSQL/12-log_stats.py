0771622ab9378ecb2ecc50356f947178d40510dc
totomus <tom.dibellonio@gmail.com>
2025-05-04 19:40:35 +0200

    test

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,38 +1,26 @@
 #!/usr/bin/env python3
-"""
-Database: logs
-Collection: nginx
-Display (same as the example):
-first line: x logs where x is the number of documents in this collection
-second line: Methods:
-5 lines with the number of documents with the method =
-["GET", "POST", "PUT", "PATCH", "DELETE"] in this order
-(see example below - warning: it’s a tabulation before each line)
-one line with the number of documents with:
-method=GET
-path=/status
-"""
+"""Provides some stats about Nginx logs stored in MongoDB."""
+
 from pymongo import MongoClient
 
+def log_stats():
+    """Displays stats about Nginx logs stored in MongoDB."""
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
 
-def helper(a: dict) -> int:
-    """return log"""
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs = client.logs.nginx
-    return logs.count_documents(a)
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
 
-
-def main():
-    """provides some stats about Nginx logs stored in MongoDB """
-    print(f"{helper({})} logs")
     print("Methods:")
-    print(f"\tmethod GET: {helper({'method': 'GET'})}")
-    print(f"\tmethod POST: {helper({'method': 'POST'})}")
-    print(f"\tmethod PUT: {helper({'method': 'PUT'})}")
-    print(f"\tmethod PATCH: {helper({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {helper({'method': 'DELETE'})}")
-    print(f"{helper({'method': 'GET', 'path': '/status'})} status check")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_checks = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_checks} status check")
 
 
 if __name__ == "__main__":
-    main()
+    log_stats()
