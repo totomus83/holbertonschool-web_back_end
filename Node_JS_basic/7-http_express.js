581ff86f1d3f184b987b3ea6c36c7f533478217c
totomus <tom.dibellonio@gmail.com>
2025-05-23 12:12:06 +0200

    more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,26 +1,65 @@
+// 7-http_express.js
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const students = data.split('\n').filter((student) => student.trim() !== '');
+      if (students.length === 0) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      students.shift();
+
+      let response = `Number of students: ${students.length}\n`;
+
+      const fields = {};
+      students.forEach((student) => {
+        const studentInfo = student.split(',');
+        if (!fields[studentInfo[3]]) {
+          fields[studentInfo[3]] = [];
+        }
+        fields[studentInfo[3]].push(studentInfo[0]);
+      });
+
+      for (const i of Object.keys(fields)) {
+        response += `Number of students in ${i}: ${fields[i].length}. List: ${fields[i].join(', ')}\n`;
+      }
+      resolve(response.trim());
+    });
+  });
+}
+
+const port = 1245;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  const databasePath = process.argv[2];
-
-  countStudents(databasePath)
-    .then(() => {
-      res.status(200).send('This is the list of our students');
-    })
-    .catch((error) => {
-      res.status(500).send(error.message);
-    });
+  try {
+    countStudents(process.argv[2])
+      .then((response) => {
+        res.send(`This is the list of our students\n${response}`);
+      })
+      .catch((error) => {
+        res.status(500).send(error.message);
+      });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
+app.listen(port, () => {
+  console.log(`Server listen on port ${port}`);
 });
 
 module.exports = app;
