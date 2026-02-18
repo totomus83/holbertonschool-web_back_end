605a5779f168db0ab9505c2822be1c297d92c0cd
totomus83 <tom.dibellonio@gmail.com>
2026-02-18 17:31:58 +0100

    added comments

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
 from base_caching import BaseCaching
-
+"""
+This module defines a simple dictionary-based cache.
+"""
 
 class BasicCache(BaseCaching):
+    
+    def __init__(self):
+        super().__init__()
+        
+     
 
     def put(self, key, item):
+        """
+        Add an item to the cache.
+        If key or item is None, return nothing.
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Retrieve an item from the cache.
+        If key is None or not in cache, return nothing.
+        """
         if key is None:
             return None
         return self.cache_data.get(key)
