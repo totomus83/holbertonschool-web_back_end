ec524d697b88ab004838305cd93fc5e690e47d99
totomus83 <tom.dibellonio@gmail.com>
2026-02-18 18:29:03 +0100

    pycodestyle test

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -21,7 +21,8 @@ class BasicCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        self.cache_data[key] = item
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """
