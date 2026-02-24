e67dc59f87b40232ab62333667d58f975a2eae67
totomus83 <tom.dibellonio@gmail.com>
2026-02-24 20:59:43 +0100

    Task 2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,6 +8,7 @@ import re
 from typing import List
 import logging
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
@@ -41,3 +42,15 @@ class RedactingFormatter(logging.Formatter):
         message = super().format(record)
         return filter_datum(self.fields, self.REDACTION, message,
                             self.SEPARATOR)
+
+def get_logger() -> logging.Logger:
+    """Return a logger that redacts PII fields."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    logger.addHandler(stream_handler)
+
+    return logger
