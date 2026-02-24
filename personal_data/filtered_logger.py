5de3b27c31b6fd50216b459e2ab26be2963b9ac1
totomus83 <tom.dibellonio@gmail.com>
2026-02-24 20:32:02 +0100

    Task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,7 +8,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     Return the log message with specified fields obfuscated.
     """
