b2e667189aba07fbd9c09510e51364176d5caad3
totomus83 <tom.dibellonio@gmail.com>
2026-02-25 17:16:16 +0100

    test

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -14,9 +14,7 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                 separator: str) -> str:
-    """
-    Return the log message with specified fields obfuscated.
-    """
+    """Return the log message with specified fields obfuscated."""
     pattern = rf"({'|'.join(fields)})=([^{separator}]+)"
     return re.sub(pattern, r"\1=" + redaction, message)
 
