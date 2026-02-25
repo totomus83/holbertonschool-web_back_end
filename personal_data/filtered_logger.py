61a707fd108ed74227766e3759cbdd27238bec39
totomus83 <tom.dibellonio@gmail.com>
2026-02-25 17:23:26 +0100

    test

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -16,6 +16,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     pattern = rf"({'|'.join(fields)})=([^{separator}]+)"
     return re.sub(pattern, r"\1=" + redaction, message)
 
+
 class RedactingFormatter(logging.Formatter):
     """Redacting Formatter class."""
 
