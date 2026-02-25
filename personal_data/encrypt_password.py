f407a125e0573b2d40e3352c78b04ad4c5d64685
totomus83 <tom.dibellonio@gmail.com>
2026-02-25 23:23:15 +0100

    pycode

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -14,6 +14,7 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password_bytes, salt)
     return hashed
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check if the provided password matches the hashed password."""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
