828211305bad654c9d55148999ed7a083ee5546b
totomus83 <tom.dibellonio@gmail.com>
2026-02-25 17:21:47 +0100

    test

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,7 @@
 """Module for filtering sensitive fields from log messages
 and connecting to a secure MySQL database."""
 
-import os
+import logging
 import re
 from typing import List
 
@@ -15,3 +15,21 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """
     pattern = rf"({'|'.join(fields)})=([^{separator}]+)"
     return re.sub(pattern, r"\1=" + redaction, message)
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize formatter with fields to redact."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the record after redacting sensitive fields."""
+        original = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            original, self.SEPARATOR)
