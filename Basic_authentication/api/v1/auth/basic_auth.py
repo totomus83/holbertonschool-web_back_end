a1d0aa9aea182f66679029dd9a68653df36a38d3
totomus83 <tom.dibellonio@gmail.com>
2026-02-27 15:38:30 +0100

    Task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,7 +8,8 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+        self, authorization_header: str) -> str:
         """
         Returns the Base64 part of the Authorization header
         """
