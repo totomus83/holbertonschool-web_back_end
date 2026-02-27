069a4d44ddb18af80ff17786e7805e595d447012
totomus83 <tom.dibellonio@gmail.com>
2026-02-27 15:49:46 +0100

    Task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,3 +60,27 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+    
+    def user_object_from_credentials(self, user_email: str, user_pwd: str):
+        """
+        Returns the User instance based on email and password
+        """
+
+        from models.user import User
+
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({"email": user_email})
+        if not users:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
