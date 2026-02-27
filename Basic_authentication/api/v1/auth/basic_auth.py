b2ebbf6446a125b2497f837fd935378db0cb3191
totomus83 <tom.dibellonio@gmail.com>
2026-02-27 15:51:26 +0100

    Task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,7 +60,7 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
-    
+
     def user_object_from_credentials(self, user_email: str, user_pwd: str):
         """
         Returns the User instance based on email and password
