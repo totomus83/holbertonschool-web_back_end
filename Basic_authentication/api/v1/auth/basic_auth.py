57e057116b4ba5077a2d562a286f2631e2f602d0
totomus83 <tom.dibellonio@gmail.com>
2026-02-27 15:48:12 +0100

    Task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,3 +42,21 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except Exception:
             return None
+        
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Returns the user email and password from a Base64 decoded string
+        """
+
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
