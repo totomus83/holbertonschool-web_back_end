0ca57628c5f14609342bcb74d3a467fc78c3e7fd
totomus83 <tom.dibellonio@gmail.com>
2026-02-27 15:46:55 +0100

    Task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -26,7 +26,6 @@ class BasicAuth(Auth):
 
         return authorization_header[len("Basic "):]
 
-
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
         """
