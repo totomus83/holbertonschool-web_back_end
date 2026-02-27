5880864752025263286d892c014a10d24364b072
totomus83 <tom.dibellonio@gmail.com>
2026-02-27 03:53:37 +0100

    Task 5

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -30,9 +30,13 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """
-        Returns None for now
+        Returns the Authorization header from the request
         """
-        return None
+        if request is None:
+            return None
+        if "Authorization" not in request.headers:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
