bdbfe72307fbc3f45674fada320bc4c0476c687d
totomus83 <tom.dibellonio@gmail.com>
2026-02-27 03:52:36 +0100

    Task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,9 +9,24 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Returns False for now
+        Returns True if authentication is required
         """
-        return False
+
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # Ensure path ends with /
+        if not path.endswith("/"):
+            path += "/"
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
