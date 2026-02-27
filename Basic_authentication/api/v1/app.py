9fcfc51a778a679523b109969235540b6e2b79ab
totomus83 <tom.dibellonio@gmail.com>
2026-02-27 03:56:22 +0100

    Task 5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -9,33 +9,36 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
 import os
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-# ---------- AUTHENTICATION SETUP ----------
+
 auth = None
 if os.getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-# ---------- ERROR HANDLERS ----------
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler."""
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """Unauthorized handler."""
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
 
-# ---------- BEFORE REQUEST ----------
+
 @app.before_request
 def before_request_handler():
     """ Validate each request before routing """
@@ -48,19 +51,16 @@ def before_request_handler():
         '/api/v1/forbidden/'
     ]
 
-    # Skip authentication if path is excluded
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    # 401 Unauthorized if no Authorization header
     if auth.authorization_header(request) is None:
         abort(401)
 
-    # 403 Forbidden if no current user
     if auth.current_user(request) is None:
         abort(403)
 
-# ---------- RUN SERVER ----------
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
