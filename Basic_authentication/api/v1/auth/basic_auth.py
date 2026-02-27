cbf54d026aa45311aa20b93cb261d35c36a92724
totomus83 <tom.dibellonio@gmail.com>
2026-02-27 15:42:39 +0100

    Task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,7 @@
 
 
 from api.v1.auth.auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth """
@@ -24,3 +24,21 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
+
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
