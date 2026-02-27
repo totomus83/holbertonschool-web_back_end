670ded071f906a9f5e7e8f9a7d0ddd1af812eac2
totomus83 <tom.dibellonio@gmail.com>
2026-02-27 03:43:51 +0100

    Task 1

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -2,6 +2,8 @@
 """
 Route module for the API
 """
+
+
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -16,9 +18,14 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler."""
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler."""
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
