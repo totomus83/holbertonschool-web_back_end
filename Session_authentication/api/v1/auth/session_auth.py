11ce450a174a7f77aab7521e2f991e859602b62b
totomus83 <tom.dibellonio@gmail.com>
2026-03-04 14:01:31 +0100

    task 2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 """ Session Authentication Module """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
+
 
 class SessionAuth(Auth):
-    """ Empty session-based authentication class """
-    pass
+    """ Session-based authentication class """
+    
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a Session ID for a given user_id
+        Args:
+            user_id (str): User ID
+        Returns:
+            str: Session ID or None if invalid
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
