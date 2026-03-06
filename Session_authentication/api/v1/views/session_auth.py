892ce6a9a61920b0262522d90e177eaaf59b075e
totomus83 <tom.dibellonio@gmail.com>
2026-03-06 15:06:44 +0100

    task 8

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -5,6 +5,7 @@ from flask import request, jsonify, make_response
 from api.v1.views import app_views
 from models.user import User
 import os
+from flask import request, jsonify, make_response, abort
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -40,3 +41,15 @@ def login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Logout route for session authentication."""
+
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
