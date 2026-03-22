389adadd13c1c0854f037e8650b6d479f69e2b87
totomus83 <tom.dibellonio@gmail.com>
2026-03-22 23:49:11 +0100

    task 2

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -53,8 +53,7 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
-    
-    
+
     def find_user_by(self, **kwargs) -> User:
         """
         Find a user by given attributes
