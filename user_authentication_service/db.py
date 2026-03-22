5a8394cd3f09ad4bc399e835c1ef2155d1adc9e5
totomus83 <tom.dibellonio@gmail.com>
2026-03-22 23:43:48 +0100

    task 1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,6 +6,7 @@ DB module
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
