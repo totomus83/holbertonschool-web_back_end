b03e821d2e6f3d25e88b50e7730ddfab32dfdf1f
totomus83 <tom.dibellonio@gmail.com>
2026-03-22 23:45:38 +0100

    task 1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -40,8 +40,13 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """
         Add a new user to the database
-        Args: email (str): user's email, hashed_password (str): user's hashed password
-        Returns: User: the created user object
+
+        Args:
+            email (str): user's email
+            hashed_password (str): user's hashed password
+
+        Returns:
+            User: the created user object
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
