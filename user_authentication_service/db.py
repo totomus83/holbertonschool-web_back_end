49e204f1bb3d6cd357b7ef553f97eb57dba66fef
totomus83 <tom.dibellonio@gmail.com>
2026-03-22 23:51:06 +0100

    test pycode

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python3
-"""
-DB module
-"""
-
+"""Database module."""
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Any
 
 from user import Base, User
 
 
 class DB:
-    """
-    DB class for managing database operations
-    """
+    """Database handler."""
 
     def __init__(self) -> None:
-        """
-        Initialize a new DB instance
-        """
+        """Create a new database instance."""
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -27,42 +23,34 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """
-        Memoized session object
-
-        Returns:
-            Session: SQLAlchemy session instance
-        """
+        """Return the current session."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Add a new user to the database
-
-        Args:
-            email (str): user's email
-            hashed_password (str): user's hashed password
-
-        Returns:
-            User: the created user object
-        """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        """Add a user and return it."""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return new_user
+        return user
 
     def find_user_by(self, **kwargs) -> User:
-        """
-        Find a user by given attributes
-        """
+        """Find a user by given fields."""
         try:
-            # Effectue une requête "SELECT * FROM users" et retourne le res
             user = self._session.query(User).filter_by(**kwargs).one()
             return user
-        except NoResultFound:  # Raised par SQLAlchemy si aucun résultat trouvé
+        except NoResultFound:
             raise NoResultFound
-        except InvalidRequestError:  # Raised par SQLAlchemy si kwarg invalide
+        except InvalidRequestError:
             raise InvalidRequestError
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update user attributes."""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if key not in User.__table__.columns.keys():
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
