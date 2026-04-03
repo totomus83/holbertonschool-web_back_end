ec85a0434e2a33c1cf44e57a42d2644cece3c1da
totomus83 <tom.dibellonio@gmail.com>
2026-04-03 18:48:53 +0200

    Task 10

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,11 @@
+-- Create a function SafeDiv to safely divide two numbers
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS DECIMAL(10,6)
+DETERMINISTIC
+BEGIN
+    RETURN IF(b = 0, 0, a / b);
+END$$
+
+DELIMITER ;
